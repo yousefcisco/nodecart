c5a0e5bd035ce7f3a10789445c85a468a7e19529
Yousef Cisco <yousefcisco@gmail.com>
2015-03-12 22:53:40 +0000

    Initial service setup

---
+++ nodecart.js
@@ -0,0 +1,10 @@
+// NodeCart.js 0.0.1
+// -----------------
+
+var _ = require('lodash');
+
+var NodeCart = function NodeCart(options) {
+    options = options || {};
+}
+
+module.exports = NodeCart;
